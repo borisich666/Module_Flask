26903cae0ea3abf0a50ad6aad47bf06fda772a12
borisich666 <160366693+borisich666@users.noreply.github.com>
2025-06-15 19:14:55 +0300

    Flask. Урок 5

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from flask import Flask
 # Создаем объект приложения Flask
 app = Flask(__name__)
 
+
 # Определяем маршрут и привязываем его к функции
 @app.route('/')
 def hello():
     return "Hello, Flask!"
+
 
 # Запуск приложения
 if __name__ == "__main__":
